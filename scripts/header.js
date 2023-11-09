18ae78c3e3b07d6920d9819c73867b2037a1371e
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2023-11-09 12:54:29 -0500

    Add presidency page

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,8 @@
 document.body.innerHTML = `<div id="header">
 <a href="/"><img id="logo" src="/images/ad-club-logo-for-website.png"></a>
 <div id="button-header-div">
-    <a href="/ad-dollars"><button id="ad-dollars-button">AD Dollars</button></a>  
+    <a href="/ad-dollars"><button>AD Dollars</button></a>
+    <a href="/presidency"><button>Presidency</button></a>
 </div>
 </div>
 
