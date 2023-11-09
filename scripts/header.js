47e62cd2c8d7415c1111d4182d94b4162eed2c4d
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2023-11-09 11:03:05 -0500

    Add header and default page

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+document.body.innerHTML = `<div id="header">
+<a href="/"><img id="logo" src="/images/ad-club-logo-for-website.png"></a>
+<div id="button-header-div">
+    <a href="/ad-dollars"><button id="ad-dollars-button">AD Dollars</button></a>  
+</div>
+</div>
+
+<div class="break"></div>
+
+<script src="scripts/header.js"></script>` + document.body.innerHTML;
