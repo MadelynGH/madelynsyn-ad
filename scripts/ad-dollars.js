82dfd22406402073c02290808d3b73df3204f9e8
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-02-17 09:42:33 -0500

    Add Madelyn and Shared AD to AD Dollars

--- scripts/ad-dollars.js
+++ scripts/ad-dollars.js
@@ -2,11 +2,17 @@ import db from "./firebase.js";
 
 const andrewADDisplayAmount = document.getElementById("andrew-ad-display");
 const danielADDisplayAmount = document.getElementById("daniel-ad-display");
+const madelynADDisplayAmount = document.getElementById("madelyn-ad-display");
+const sharedADDisplayAmount = document.getElementById("shared-ad-display");
 
 let andrewId;
 let danielId;
+let madelynId;
+let sharedADId;
 let andrewAmount;
 let danielAmount;
+let madelynAmount;
+let sharedADAmount;
 
 const andrewNumber = document.getElementById("andrew-number");
 const andrewAddButton = document.getElementById("andrew-add");
@@ -15,6 +21,14 @@ const andrewTakeAwayButton = document.getElementById("andrew-take-away");
 const danielNumber = document.getElementById("daniel-number");
 const danielAddButton = document.getElementById("daniel-add");
 const danielTakeAwayButton = document.getElementById("daniel-take-away");
+
+const madelynNumber = document.getElementById("madelyn-number");
+const madelynAddButton = document.getElementById("madelyn-add");
+const madelynTakeAwayButton = document.getElementById("madelyn-take-away");
+
+const sharedADNumber = document.getElementById("shared-ad-number");
+const sharedADAddButton = document.getElementById("shared-ad-add");
+const sharedADTakeAwayButton = document.getElementById("shared-ad-take-away");
 
 // Display real-time amount for Andrew
 db.collection("users").where("name", "==", "andrew")
@@ -43,6 +57,40 @@ db.collection("users").where("name", "==", "daniel")
                 danielId = doc.id;
                 danielAmount = doc.data().ad_dollars;
                 danielADDisplayAmount.innerHTML = "Daniel's AD Dollars: " + doc.data().ad_dollars;
+            });
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+    
+// Display real-time amount for Madelyn
+db.collection("users").where("name", "==", "madelyn")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            db.collection("users").doc(doc.id)
+            .onSnapshot((doc) => {
+                madelynId = doc.id;
+                madelynAmount = doc.data().ad_dollars;
+                madelynADDisplayAmount.innerHTML = "Madelyn's AD Dollars: " + doc.data().ad_dollars;
+            });
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+// Display real-time amount for Shared AD
+db.collection("users").where("name", "==", "shared_ad")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            db.collection("users").doc(doc.id)
+            .onSnapshot((doc) => {
+                sharedADId = doc.id;
+                sharedADAmount = doc.data().ad_dollars;
+                sharedADDisplayAmount.innerHTML = "Shared AD Dollars: " + doc.data().ad_dollars;
             });
         });
     })
@@ -79,6 +127,10 @@ const takeAwayAD = function(amount, personId, person, personAmountVariable) {
 
 andrewAddButton.addEventListener("click", function() {addAD(andrewNumber.value, andrewId, "andrew", andrewAmount); andrewNumber.value = "";})
 danielAddButton.addEventListener("click", function() {addAD(danielNumber.value, danielId, "daniel", danielAmount); danielNumber.value = "";})
+madelynAddButton.addEventListener("click", function() {addAD(madelynNumber.value, madelynId, "madelyn", madelynAmount); madelynNumber.value = "";})
+sharedADAddButton.addEventListener("click", function() {addAD(sharedADNumber.value, sharedADId, "shared_ad", sharedADAmount); sharedADNumber.value = "";})
 
 andrewTakeAwayButton.addEventListener("click", function() {takeAwayAD(andrewNumber.value, andrewId, "andrew", andrewAmount); andrewNumber.value = "";})
 danielTakeAwayButton.addEventListener("click", function() {takeAwayAD(danielNumber.value, danielId, "daniel", danielAmount); danielNumber.value = "";})
+madelynTakeAwayButton.addEventListener("click", function() {takeAwayAD(madelynNumber.value, madelynId, "madelyn", madelynAmount); madelynNumber.value = "";})
+sharedADTakeAwayButton.addEventListener("click", function() {takeAwayAD(sharedADNumber.value, sharedADId, "shared_ad", sharedADAmount); sharedADNumber.value = "";})
